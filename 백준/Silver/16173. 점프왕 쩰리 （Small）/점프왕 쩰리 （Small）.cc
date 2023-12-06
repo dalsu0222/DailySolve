531e52f5a5d6962b71c38716098962b9509da024
신아진 <97183032+dalsu0222@users.noreply.github.com>
2023-12-06 21:52:01 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+int N;
+int map[3][3];
+int visited[3][3];
+//string result = "";
+queue <pair<int,int>> q;
+int dx[2] = { 0,1 };
+int dy[2] = { 1,0 };
+
+bool bfs(int i, int j) {	
+	visited[i][j] = 1;
+	q.push(make_pair(i,j));
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 2; i++) {
+			int new_x = x + dx[i] * map[x][y];		// cnt = map[x][y]
+			int new_y = y + dy[i] * map[x][y];
+			if (0 <= new_x && 0 <= new_y && new_x < N && new_y < N 
+				&& visited[new_x][new_y] == 0) {
+				
+				if (map[new_x][new_y] == -1) {
+					return true;
+				}
+				q.push(make_pair(new_x, new_y));
+				visited[new_x][new_y] = 1;
+			}
+		}
+	}
+	return false;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	if (bfs(0, 0))
+		cout << "HaruHaru";
+	else
+		cout << "Hing";
+
+	return 0;
+}
