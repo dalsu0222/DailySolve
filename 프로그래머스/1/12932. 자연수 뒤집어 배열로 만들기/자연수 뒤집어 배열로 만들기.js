97208bb6ce062dd8e347e9f2ef3573c06d6e8de8
신아진 <97183032+dalsu0222@users.noreply.github.com>
2024-06-19 23:27:45 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    var answer = [];
-    const s = String(n);
-    for(var i=s.length-1 ; i>=0 ; i--){
-        var num = parseInt(s[i]);
-        answer.push(num);
-    }
+    const answer = [];
+    
+    do{
+        answer.push(n%10);
+        n = Math.trunc(n/10);   // -를 제외하고 소숫점 버림, 정수 반환. -를 제외하므로 음수 양수 동일하게 작동
+    }while(n>0)
+    
     return answer;
 }
