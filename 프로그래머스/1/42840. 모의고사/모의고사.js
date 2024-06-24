c2d47bd11a1f6b069bb14ee493eed81bb1e5267e
신아진 <97183032+dalsu0222@users.noreply.github.com>
2024-06-24 23:13:45 +0900

    [level 1] Title: 모의고사, Time: 5.87 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,35 @@
+var index = 0;  // arr1,2,3 을 순회하는 인덱스
+function solution(answers) {
+  var arr1 = [1, 2, 3, 4, 5];
+  var arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  var arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  var cnt1 = 0;
+  var cnt2 = 0;
+  var cnt3 = 0;
+  for (var i = 0; i < answers.length; i++) {
+    if (answers[i] === arr1[index % 5]) {
+      cnt1++;
+    }
+    if (answers[i] === arr2[index % 8]) {
+      cnt2++;
+    }
+    if (answers[i] === arr3[index % 10]) {
+      cnt3++;
+    }
+    index++;
+  }
+  var answer = [];
+  var max_c = 0;
+  var tmp = [];
+  tmp.push(cnt1);
+  tmp.push(cnt2);
+  tmp.push(cnt3);
+  for (var i = 0; i < 3; i++) { // 최고점 찾기
+    if (max_c < tmp[i]) max_c = tmp[i];
+  }
+  for (var i = 0; i < 3; i++) { // 최고점을 맞힌 사람 찾기
+    if (max_c === tmp[i]) answer.push(i + 1);
+  }
+  console.log(answer);
+  return answer;
+}
