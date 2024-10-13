62dcefdc4130af0576e89afcfb5e713079bb8047
신아진 <97183032+dalsu0222@users.noreply.github.com>
2024-10-13 16:19:11 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 8 ms, Memory: 2260 KB -BaekjoonHub

---
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+int n, k;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> k;
+	vector<int> v(n);
+	vector<bool> in(n, true);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	int result = 0;	// 짝수로 이루어진 가장 긴 부분 수열의 길이
+	int cnt = 0;	// 현재 구간의 홀수 개수
+	int l = 0, r = 0;
+	while (r < n) {
+		if (v[r++] % 2 == 1) {
+			cnt++;
+		}
+		// 홀수 개수가 K를 초과하면 왼쪽 포인터 이동
+		while (cnt > k) {
+			if (v[l++] % 2 == 1) {
+				cnt--;
+			}
+		}
+
+		// 짝수 부분 수열의 길이 갱신
+		result = max(result, r - l - cnt);
+	}
+
+	cout << result;
+
+	return 0;
+}
