4e823958d731ee6eba4493487752ec078ef8273a
신아진 <97183032+dalsu0222@users.noreply.github.com>
2025-02-21 00:42:49 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 108 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
-int main(){
+int main() {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int N,M;
-    map<string,string> m;        // map(파이썬의 딕셔너리와 비슷) 자료구조 활용
+    int N, M;
+    unordered_map<string, string> m;
     string name, pw;
 
     cin >> N >> M;
 
-    for(int i=0;i<N;i++){
+    for (int i = 0; i < N; i++) {
         cin >> name >> pw;
-
         m[name] = pw;
     }
 
-    for(int i=0;i<M;i++){
+    for (int i = 0; i < M; i++) {
         cin >> name;
         cout << m[name] << '\n';
     }
