6f91e657db428fd91f3b05f9376a21ae4dfd6ef0
신아진 <97183032+dalsu0222@users.noreply.github.com>
2025-07-09 00:41:39 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,9 @@
+select CONCAT('/home/grep/src/',f.BOARD_ID, '/',f.FILE_ID,f.FILE_NAME,f.FILE_EXT) as FILE_PATH
+from USED_GOODS_FILE f
+join USED_GOODS_BOARD b
+on f.BOARD_ID = b.BOARD_ID
+where b.VIEWS = ( 
+        select MAX(VIEWS) 
+        from USED_GOODS_BOARD
+      )
+order by f.FILE_ID desc;
