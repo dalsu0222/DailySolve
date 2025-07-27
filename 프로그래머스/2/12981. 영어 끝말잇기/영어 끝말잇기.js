9ba17437c815350f3bcb71bb296a19d1517095e7
신아진 <97183032+dalsu0222@users.noreply.github.com>
2025-07-27 17:10:19 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,28 +1,14 @@
 function solution(n, words) {
-  if (words[0].length < 2) {
-    // 첫번째 단어가 탈락일 조건
-    return [1, 0];
-  }
-
-  let turn = 1;
-  let isExist = new Set(); // 중복단어 검사하기위한 단어 저장소
-  isExist.add(words[0]);
-
-  for (let i = 1; i < words.length; i++) {
-    if (i % n == 0) {
-      // n번째 단어마다 턴 증가
-      turn++;
+    const isWord = {};
+    isWord[words[0]] = true;
+    for(let i=1 ; i<words.length ; i++){
+        const word= words[i];
+        const beforeEnd =  words[i-1].charAt(words[i-1].length-1);
+        if(isWord[word] || beforeEnd !== word.charAt(0) ){
+            return [i%n + 1 , Math.floor(i/n) + 1];
+        }else{
+            isWord[word] = true;
+        }
     }
-    if (
-      // 끝말잇기 종료조건
-      words[i].length < 2 ||
-      words[i - 1][words[i - 1].length - 1] !== words[i][0] ||
-      isExist.has(words[i])
-    ) {
-      return [(i % n) + 1, turn]; // 인덱스 기준 0~n-1 번째 사람이므로, +1 하여 반환
-    }
-    isExist.add(words[i]); // 단어 저장소에 추가
-  }
-
-  return [0, 0]; // 종료조건에 걸린것이 없다면, [0,0] 반환
+    return [0,0];
 }
