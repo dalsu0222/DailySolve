0f98c3c877eec4b4a3ee996dea9ee548a0e2f6bd
신아진 <97183032+dalsu0222@users.noreply.github.com>
2025-09-26 18:12:12 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -1,10 +1,10 @@
 select i.ITEM_ID, i.ITEM_NAME, i.RARITY
-from ITEM_TREE t
-join ITEM_INFO i on t.ITEM_ID = i.ITEM_ID
-where t.ITEM_ID not in (
+from ITEM_INFO i
+join ITEM_TREE t on i.ITEM_ID = t.ITEM_ID
+where i.ITEM_ID not in (
     select DISTINCT PARENT_ITEM_ID
-    from ITEM_TREE 
+    from ITEM_TREE
     where PARENT_ITEM_ID is NOT NULL
 )
-order by t.ITEM_ID desc
+order by ITEM_ID desc
 ;
