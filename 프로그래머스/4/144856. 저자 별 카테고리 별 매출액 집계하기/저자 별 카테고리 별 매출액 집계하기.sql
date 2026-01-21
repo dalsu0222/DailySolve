43c9a2289a7cc7b73750d3d5237a8b21bdfdb10f
신아진 <97183032+dalsu0222@users.noreply.github.com>
2026-01-22 00:18:23 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,8 +1,10 @@
-select b.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, sum(b.PRICE * s.SALES) as TOTAL_SALES
+select b.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, 
+sum(b.PRICE * s.SALES) # join된 행마다 계산 됨, 나중에 group by로 합침
+as TOTAL_SALES
 from BOOK b
-join BOOK_SALES s on b.BOOK_ID = s.BOOK_ID
-left join AUTHOR a on a.AUTHOR_ID = b.AUTHOR_ID
-where s.SALES_DATE between '2022-01-01' and '2022-01-31'
-group by a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
-order by b.AUTHOR_ID asc, b.CATEGORY desc
+join AUTHOR a on a.AUTHOR_ID = b.AUTHOR_ID
+join BOOK_SALES s on s.BOOK_ID = b.BOOK_ID # book_sales 에서 같은 book_id의 행 개수 만큼 최종 테이블에서 같은 book_id를 가진 행이 생김.
+where s.SALES_DATE >= '2022-01-01' and s.SALES_DATE <  '2022-02-01'
+group by b.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
+order by b.AUTHOR_ID, b.CATEGORY desc
 ;
